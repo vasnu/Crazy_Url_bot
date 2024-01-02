421b5e5eab40a5170aed936c5998f88f311dc88d
vasnu <154647578+vasnu@users.noreply.github.com>
2024-01-02 13:44:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/vasnu/Crazy_Url_bot /AngleSonaliURLBOT
+cd /AngleSonaliURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
